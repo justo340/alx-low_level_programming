667a96c04db9e26607dd09e95fca2aa81c0abab9
njogu <justusnjogu6@gmail.com>
2022-09-28 07:37:30 -0700

    print string followd by new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: pointer to string
@@ -9,7 +10,7 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 		putchar(*s);
-		puts_recursion(s+1);
+		_puts_recursion(s+1);
 	}
 	else
 		putchar('\n');
