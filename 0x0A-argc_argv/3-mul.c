a9cfa7fe609759662415f9e8d72bf89a361aac39
njogu <justusnjogu6@gmail.com>
2022-09-30 10:02:24 -0700

    multiply two arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,12 +9,13 @@
 int main(int argc, char *argv[])
 {
 	int a, b, result;
+	
 	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
-											}
-			/* atoi convert a string to an int */
+	}
+	/* atoi convert a string to an int */
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
 	result = a * b;
