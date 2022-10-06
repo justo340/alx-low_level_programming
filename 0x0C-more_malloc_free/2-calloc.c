7dc7855a198544509d999deb9a394166580d9cae
njogu <justusnjogu6@gmail.com>
2022-10-06 12:17:19 -0700

    allocate memory for array using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,11 +14,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	
 	space = malloc(nmemb * size);
 	if (space == NULL)
 		return (NULL);
-
 	for (; i < nmemb * size; i++)
 		*(space + i) = 0;
 
