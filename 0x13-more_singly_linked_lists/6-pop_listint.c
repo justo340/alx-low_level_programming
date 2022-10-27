d0606fc92a09da7cabdc7483d1f640210f2db4ee
njogu <justusnjogu6@gmail.com>
2022-10-27 13:54:50 -0700

    remove hea and return heads data

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- *  * pop_listint - A function that deletes the head node of a list
- *   * and returns the head node's data n.
- *    * @head: A pointer to listint_t structure.
- *     * Return: The head node's datafrom deleted element
- *      */
+ * pop_listint - A function that deletes the head node of a list
+ * and returns the head node's data n.
+ * @head: A pointer to listint_t structure.
+ * Return: The head node's datafrom deleted element
+ */
 int pop_listint(listint_t **head)
 {
 	listint_t *new_node;
