912eddccd352e911383718ad6bdcedcacc18700d
njogu <justusnjogu6@gmail.com>
2022-10-31 13:38:48 -0700

    covert binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,8 +18,9 @@ unsigned int binary_to_uint(const char *b)
 
 	while (len)
 	{
-		if (b[len - 1] != '0' && b[len - 1] != '1')						return (0);
-
+		if (b[len - 1] != '0' && b[len - 1] != '1')
+			return (0);
+		
 		if (b[len - 1] == '1')
 			result += base;
 		base *= 2;
